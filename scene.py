1580e677b29abc7e4ea828fcd641eb40a159e105
taKana671 <kanae5321@gmail.com>
2026-03-07 14:17:48 +0900

    Changed to creage garden.

--- scene.py
+++ scene.py
@@ -4,80 +4,136 @@ import random
 import numpy as np
 from panda3d.core import NodePath, PandaNode
 from panda3d.core import Point3, LColor, Vec3
-from panda3d.core import TextureStage, Texture
+from panda3d.core import TextureStage, Texture, TransformState
+from shapely.geometry import Polygon
 
 from shapes import RandomPolygonalPrism
-from shapes import Plane
+from shapes import Plane, Cylinder
+
 from voronoi_region_generator import BoundedVoronoiGenerator, ConvexPolygonGenerator
+from polygon_mixin import PolygonMixin
 
 
-class SquareTownBuilder:
+class PineTree(NodePath):
+
+    def __init__(self, model, name, scale=2):
+        super().__init__(PandaNode(name))
+        tree = model.copy_to(self)
+        tree.set_transform(TransformState.make_pos(Vec3(0, 0, -4)))
+        tree.reparent_to(self)
+        self.set_scale(scale)
+
+
+class SquareTownBuilder(PolygonMixin):
 
     def __init__(self, scale=256):
         self.scale = scale
         self.foundation_tex = base.loader.load_texture('textures/foundation_2.png')
         self.building_tex = base.loader.load_texture('textures/building.png')
         self.roof_tex = base.loader.load_texture('textures/metal_02.png')
+        self.spot_tex = base.loader.load_texture('textures/concrete_01.jpg')
+        self.grass_tex = base.loader.load_texture('textures/grass_04.jpg')
+        self.tree_model = base.loader.load_model('models/pinetree/tree2.bam')
 
     def round_off(self, number, ndigits=0):
         p = 10 ** ndigits
         return (number * p * 2 + 1) // 2 / p
 
-    def sort_counter_clockwise(self, arr):
-        center = np.mean(arr, axis=0)
-        angles = np.arctan2(arr[:, 1] - center[1], arr[:, 0] - center[0])
-        sorted_indices = np.argsort(angles)
-        sorted_pts = arr[sorted_indices]
-        return sorted_pts
-
     def build(self):
-        for region in BoundedVoronoiGenerator():
+        for i, region in enumerate(BoundedVoronoiGenerator()):
             poly_pts = np.array([pt for pt in ConvexPolygonGenerator(region)])
 
-            for i, pts in enumerate(BoundedVoronoiGenerator(pts=poly_pts, bnd=region, shrink=0.003)):
+            for j, pts in enumerate(BoundedVoronoiGenerator(pts=poly_pts, bnd=region, shrink=0.003)):
                 polygon = np.insert(pts, pts.shape[1], 0, axis=1)
+                serial = f'{i}_{j}'
+
+                if j == 0:
+                    if nd := self.create_green(polygon, serial):
+                        yield nd
+                        continue
+
                 sorted_pts = self.sort_counter_clockwise(polygon)
-                yield self.create_building(sorted_pts, i)
+                yield self.create_building(sorted_pts, serial)
+
+    def create_green(self, sorted_pts, serial):
+        center, radius = self.get_max_inscribed_circle(sorted_pts)
+        spot_rad = radius * self.scale
+        inner_radius = spot_rad - 0.5
+        height = 0.001 * self.scale
+
+        # If the radius of a circular garden is too small, do not create the garden.
+        if (n := int(inner_radius) - 2) <= 0:
+            return None
+
+        nd = NodePath(f'garden_{serial}')
+        # Create the edge of the circular garden.
+        spot = Cylinder(spot_rad, inner_radius=inner_radius, height=height).create()
+        pos = Point3(*center, 0) * self.scale
+        spot.set_texture(self.spot_tex)
+        spot.set_pos(pos)
+        spot.reparent_to(nd)
+
+        # Create the lawn area of the circular garden
+        green = Cylinder(inner_radius, height=height - 0.1).create()
+        green.set_pos(pos)
+        green.set_texture(self.grass_tex)
+        green.reparent_to(nd)
+
+        # Plant trees.
+        pos_candidates = random.sample(range(-n, n), 2 * n - 2)
+
+        for i in range(0, len(pos_candidates) - 1, 2):
+            x, y = pos_candidates[i: i + 2]
+            dist = (x ** 2 + y ** 2) ** 0.5
+            if dist < inner_radius:
+                tree = PineTree(self.tree_model, f'tree_{i}')
+                tree_pos = pos + Vec3(x, y, 8.5)
+                tree.set_pos(tree_pos)
+                tree.reparent_to(nd)
+
+        return nd
 
     def create_building(self, sorted_pts, serial):
         nd = NodePath(f'building_{serial}')
-        model_creator = RandomPolygonalPrism([pt for pt in sorted_pts])
-        edge_length = (model_creator.edge_length * self.scale)
-        pos = Point3(*model_creator.center * self.scale)
+        scaled_pts = sorted_pts * self.scale
+        model_creator = RandomPolygonalPrism([pt for pt in scaled_pts])
+
+        # edge_length = model_creator.edge_length
+        pos = Point3(*model_creator.center)
         ts = TextureStage.get_default()
 
         # Create building foundation.
         foundation_h = 0.04
         model_creator.height = foundation_h
         foundation = model_creator.create()
-        foundation.set_pos_hpr_scale(pos, Vec3(), self.scale)
+        foundation.set_pos(pos)
 
-        su = self.round_off(edge_length / 50)
+        su = self.round_off(model_creator.edge_length / 50)
         foundation.set_tex_scale(ts, (su, 1))
         foundation.set_texture(self.foundation_tex)
         foundation.reparent_to(nd)
 
         # Create building wall.
-        wall_h = random.randint(10, 100) / self.scale
+        wall_h = random.choice([10, 20, 30, 40, 50, 60, 70, 80])
         model_creator.height = wall_h
         wall = model_creator.create()
-        wall_pos = Point3(pos.xy, (foundation_h * self.scale))
-        wall.set_pos_hpr_scale(wall_pos, Vec3(), self.scale)
+        wall_pos = Point3(pos.xy, foundation_h)
+        wall.set_pos(wall_pos)
 
-        u = edge_length / 50
+        u = model_creator.edge_length / 50
         su = np.ceil(u * 3) / 3
-        v = (wall_h * self.scale) / 50
+        v = wall_h / 50
         sv = np.ceil(v * 4) / 4
         wall.set_tex_scale(ts, (su, sv))
         wall.set_texture(self.building_tex)
         wall.reparent_to(nd)
 
         # Create building roof.
-        model_creator.height = 3 / self.scale
+        model_creator.height = 3
         roof = model_creator.create()
         roof.set_texture(self.roof_tex)
-        roof_pos = Point3(pos.xy, (wall_h + foundation_h) * self.scale)
-        roof.set_pos_hpr_scale(roof_pos, Vec3(), self.scale)
+        roof_pos = Point3(pos.xy, wall_h + foundation_h)
+        roof.set_pos(roof_pos)
         roof.reparent_to(nd)
 
         return nd
